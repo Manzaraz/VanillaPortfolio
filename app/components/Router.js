3b025988329e6f988acc90e345124ddec404c0c2
Christian Manzaraz <magnusmanz@gmail.com>
2024-09-22 01:34:52 -0300

    Fixing listeners on index.js

--- app/components/Router.js
+++ app/components/Router.js
@@ -42,7 +42,7 @@ export function Router() {
     } else {
       $main.innerHTML = "<h2>Page Not found</h2>"
     }
-    listener()
+    // listener()
   }
 
   function updateLinks(currentPage) {
@@ -76,6 +76,6 @@ export function Router() {
   updateContent(initialPage);
   updateLinks(initialPage);
 
-  listener()
+  // listener()
 }
 
--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 import { App } from "./App.js";
+import { listener } from "./helpers/listeners.js";
 
 AOS.init();
 document.addEventListener("DOMContentLoaded", App)
+document.addEventListener("DOMContentLoaded", listener)
